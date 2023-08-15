657d8783d36365456a3db45067275426d7736c88
Fridayblessed007 <Fridayblessed221@gmail.com>
2023-08-15 14:32:55 +0100

    exercise 2

---
+++ 0x02.py/0x02.py
@@ -0,0 +1,4 @@
+hours = input("Enter Hours: ")
+rate= input("Enter rate: ")
+total = float(hours) * float(rate)
+print("pay:", total)
---
+++ 0x02.py/README.md
@@ -0,0 +1,2 @@
+# EXERCISE 2
+Write a program to prompt for hours and rate per hour to complete gross pay.
